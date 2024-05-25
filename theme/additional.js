926ed9c72aa6fc3a2cfc8dbb2db4fb6aa4d70cc4
edison-enriquez <edison.enriquez@correounivalle.edu.co>
2024-05-25 16:37:18 +0000

    update

--- theme/additional.js
+++ theme/additional.js
@@ -265,3 +265,24 @@ function setupNetwork2() {
         }
     });
 }
+
+
+/******************* */
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+// Mostrar la primera pestaña por defecto
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector('.tablinks').click();
+});
